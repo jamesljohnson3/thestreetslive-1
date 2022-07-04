ad74f270c8d7237354f25eaeb29ea39dc35d97b2
Admin <support@unlimitpotential.com>
2022-07-03 22:09:06 -0400

    k

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className="relative flex flex-col px-10 mx-auto space-y-5 md:w-3/4">
         <header className="flex items-center justify-between space-x-3">
           <Link href="/">
-            <a className="text-2xl font-bold">Unlimited Now</a>
+            <a className="text-2xl font-bold">Unlimit Potential®</a>
           </Link>
           <button className="md:hidden" onClick={toggleMenu}>
             {!showMenu ? (
@@ -51,11 +51,11 @@ const Hero = () => {
         </header>
         <div className="flex flex-col items-center justify-center pt-10 mx-auto md:w-3/5">
           <h1 className="text-6xl font-extrabold text-center">
-            <span className="block">My Unlimited </span>
-            <span className="block text-blue-600">Now Site</span>
+            <span className="block">My Site </span>
+            <span className="block text-blue-600">Unlimited Now</span>
           </h1>
           <p className="mt-5 text-center text-gray-600">
-            Quickly build landing pages that will help you get results fast
+            Save Money. Save Time.
           </p>
         </div>
         <div className="flex items-center justify-center space-x-5">
