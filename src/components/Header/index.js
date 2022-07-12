7350a2560c0f4300d64daae6381d76442f43eade
Admin <support@unlimitpotential.com>
2022-07-12 02:11:55 -0400

    g

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -66,7 +66,7 @@ const Header = () => {
                 </Link>
               </Menu.Item>
               <Menu.Item>
-                <Link href="/account/billing">
+                <Link href="https://dashboard.unlimitednow.site/signin">
                   <a className="flex items-center w-full px-2 py-2 space-x-2 text-sm text-gray-800 rounded hover:bg-blue-600 hover:text-white group">
                     <CreditCardIcon aria-hidden="true" className="w-5 h-5" />
                     <span>Billing</span>
