d60569866f0a323648195a61d237f9d3b870a152
Admin <support@unlimitpotential.com>
2022-07-12 02:22:04 -0400

    g

--- src/config/menu/sidebar-static.js
+++ src/config/menu/sidebar-static.js
@@ -8,7 +8,7 @@ const sidebarMenu = () => [
       },
       {
         name: 'Billing',
-        path: `/account/billing`,
+        path: `subscriptions`,
       },
       {
         name: 'Settings',
