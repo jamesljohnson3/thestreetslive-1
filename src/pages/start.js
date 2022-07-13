d321f4fdb221f5649b6058a467b9ee18b3fda0d7
Admin <support@unlimitpotential.com>
2022-07-13 14:04:36 -0400

    ok

---
+++ src/pages/start.js
@@ -0,0 +1,151 @@
+export default function MyComponent(props) {
+  return (
+    <>
+      <image builderTag="image" className="image">
+        <picture>
+          <source
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2"
+            type="image/webp"
+          />
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2"
+            sizes="(max-width: 638px) 100vw, (max-width: 998px) 100vw, 143vw"
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F095e5606bd8043d1aa47deeda9bc48d2"
+            className="image-2"
+          />
+        </picture>
+        <div className="builder-image-sizer image-sizer" />
+      </image>
+      <div className="div">
+        <picture>
+          <source
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38"
+            type="image/webp"
+          />
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38"
+            sizes="(max-width: 638px) 100vw, (max-width: 998px) 100vw, 143vw"
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2Fa73f8fd25c9c4e4882b2b3a2a3ad0d38"
+            className="image-3"
+          />
+        </picture>
+        <div className="builder-image-sizer image-sizer" />
+      </div>
+      <div className="div-2">
+        <picture>
+          <source
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682"
+            type="image/webp"
+          />
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682"
+            sizes="(max-width: 638px) 100vw, (max-width: 998px) 96vw, 83vw"
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Ffc8480a6998e49d7af4ee889483a2619%2F4117ab9510e24c16ab30434150892682"
+            className="image-4"
+          />
+        </picture>
+        <div className="builder-image-sizer image-sizer" />
+      </div>
+      <style jsx>{`
+        .image {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 0px;
+          width: 100vw;
+          min-height: 20px;
+          min-width: 20px;
+          overflow: hidden;
+          margin-left: calc(50% - 50vw);
+        }
+        @media (max-width: 991px) {
+          .image {
+            width: 100vw;
+            margin-left: calc(50% - 50vw);
+          }
+        }
+        .image-2 {
+          object-fit: cover;
+          object-position: center;
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          top: 0;
+          left: 0;
+        }
+        .image-sizer {
+          width: 100%;
+          padding-top: 56.3%;
+          pointer-events: none;
+          font-size: 0;
+        }
+        .div {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 0px;
+          width: 100vw;
+          min-height: 20px;
+          min-width: 20px;
+          overflow: hidden;
+          margin-left: calc(50% - 50vw);
+        }
+        @media (max-width: 991px) {
+          .div {
+            width: 100vw;
+            margin-left: calc(50% - 50vw);
+          }
+        }
+        @media (max-width: 640px) {
+          .div {
+            width: 100vw;
+            margin-left: calc(50% - 50vw);
+          }
+        }
+        .image-3 {
+          object-fit: contain;
+          object-position: center;
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          top: 0;
+          left: 0;
+        }
+        .div-2 {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 0px;
+          width: 100%;
+          min-height: 20px;
+          min-width: 20px;
+          overflow: hidden;
+        }
+        @media (max-width: 991px) {
+          .div-2 {
+            width: 100vw;
+            margin-left: calc(50% - 50vw);
+          }
+        }
+        @media (max-width: 640px) {
+          .div-2 {
+            width: 100vw;
+            margin-left: calc(50% - 50vw);
+          }
+        }
+        .image-4 {
+          object-fit: cover;
+          object-position: center;
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          top: 0;
+          left: 0;
+        }
+      `}</style>
+    </>
+  );
+}
