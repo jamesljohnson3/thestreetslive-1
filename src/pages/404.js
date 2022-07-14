d835affd47177a299db29eb7a46dd4731fb2a6bb
Admin <support@unlimitpotential.com>
2022-07-14 00:21:20 -0400

    404 last test

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,6 @@ export default function Home() {
         <section>
             <div className="min-h-screen px-4 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
               <div className="flex flex-col w-full mb-12 text-left lg:text-center">
-              <img src="/now.png" alt="Ghost" height="64" width="64" className="text-center mx-auto transform scale-110 animate-pulse	"/>
-              <h2 className="text-5xl max-w-xl mx-auto mt-8 text-left lg:text-center font-spooky animate-pulse">404 n</h2>
                 <p className="text-2xl max-w-xl mx-auto mt-4 text-left lg:text-center">Looks like you got lost in the party.</p>
                 <p className=" max-w-xl mx-auto mt-8 text-left lg:text-center">Did you try searching? Enter a keyword(s) in the search field above or try one of the links below.</p>
               </div>
