bb55ba9c5675a32d7bc725d627aeefb0602a27fa
Admin <support@unlimitpotential.com>
2022-08-31 22:51:48 -0400

    no message

---
+++ src/pages/page10.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { Widget } from "@uploadcare/react-widget";
+import { useEffect, useRef } from "react";
+import uploadcare from "uploadcare-widget";
+
+export default function App() {
+  useEffect(() => {
+    window.UPLOADCARE_PUBLIC_KEY = "demopublickey";
+    window.UPLOADCARE_SYSTEM_DIALOG = true;
+  }, []);
+
+  const ref = useRef();
+
+  const handleChange = (fileInfo) => {
+    const file = uploadcare.fileFrom("uploaded", fileInfo.uuid);
+    const dialog = uploadcare.openDialog(file, "preview", {
+      crop: "1:1",
+      tabs: "file camera"
+    });
+    dialog.done((res) => {
+      res.done((fileInfo) => {
+        ref.current.getInput().value = fileInfo.cdnUrl;
+      });
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Widget ref={ref} publicKey="demopublickey" onChange={handleChange} />
+    </div>
+  );
+}
