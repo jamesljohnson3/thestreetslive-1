8480ff7a8aa7e1e6fcf4828aa32f83373f009a88
Admin <support@unlimitpotential.com>
2023-03-24 03:34:33 -0400

    a

--- src/pages/forms/share.js
+++ src/pages/forms/share.js
@@ -76,7 +76,7 @@ const Invite = ({ workspace }) => {
                               <label className="block mb-3 text-sm font-medium text-gray-600" name="email">
                                 How shall we contact you?
                               </label>
-                              <input className="block w-full px-6 py-3 text-center text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="email@example.com" autocomplete="off" type="email"></input>
+                              <input className="block w-full px-6 py-3 text-center text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="email@example.com" type="email"></input>
                             </div>
                             <div>
                               <button type="" className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black">
