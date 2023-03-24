1cb8d880531373297a772d6616f0a5e5b9342a34
Admin <support@unlimitpotential.com>
2023-03-24 03:19:34 -0400

    k

--- src/pages/teams/invite.js
+++ src/pages/teams/invite.js
@@ -54,14 +54,12 @@ const Invite = ({ workspace }) => {
                       it engaging and interesting, and showcase your brands personality. Thanks for
                       visiting our website!
                     </p>
-                    <p>Workspace ID: {workspace.id}</p>
-                    <div>CreatorID: {workspace.creatorId}</div>
                   </div>
                   <div className="flex flex-col items-center justify-center gap-3 mt-10 lg:flex-row lg:justify-start">
                     <a href="https://my.unlimitpotential.com/865984578?utm_source=templates&utm_campaign=share%invite%link" className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none lg:w-auto focus-visible:outline-black text-sm focus-visible:ring-black">
                       Get started
                     </a>
-                    <a href={`https://${workspace.creatorId}`} className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600">
+                    <a href={`https://${workspace.id}.nowpro.shop`} className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600">
                       Preview Website
                       <span aria-hidden="true"> → </span>
                     </a>
