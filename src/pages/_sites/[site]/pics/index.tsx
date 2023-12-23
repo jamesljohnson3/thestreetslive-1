bfa51f0885b5383acf99e3ce379340e25971c5a3
Admin <support@unlimitpotential.com>
2023-12-23 17:32:35 -0500

    hj

--- src/pages/_sites/[site]/pics/index.tsx
+++ src/pages/_sites/[site]/pics/index.tsx
@@ -123,13 +123,13 @@ const DynamicPage: NextPage = ({ images }: { images: ImageProps[] }) => {
                       >
                         <Image
                           alt="Next.js Conf photo"
-                          className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                          className="transform  object-cover rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
                           style={{ transform: 'translate3d(0, 0, 0)' }}
                           placeholder="blur"
                           blurDataURL={blurDataUrl}
                           src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
                           width={720}
-                          height={480}
+                          height={720}
                           sizes="(max-width: 640px) 100vw,
                             (max-width: 1280px) 50vw,
                             (max-width: 1536px) 33vw,
