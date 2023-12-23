374d16c61aa8fb5e4eadd48dc8623cdeca41d10d
Admin <support@unlimitpotential.com>
2023-12-23 15:18:18 -0500

    h

---
+++ src/pages/_sites/[site]/pics/index.tsx
@@ -0,0 +1,179 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Modal from '../../../../components/Modal'
+import cloudinary from '../../../../utils/cloudinary'
+import getBase64ImageUrl from '../../../../utils/generateBlurPlaceholder'
+import type { ImageProps } from '../../../../utils/types'
+import { useLastViewedPhoto } from '../../../../utils/useLastViewedPhoto'
+
+const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
+  const router = useRouter()
+  const { photoId } = router.query
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto()
+
+  useEffect(() => {
+    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
+    if (lastViewedPhoto && !photoId) {
+      document
+        .querySelector(`#photo-${lastViewedPhoto}`)
+        .scrollIntoView({ block: 'center' })
+
+      setLastViewedPhoto(null)
+    }
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
+
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta
+          property="og:image"
+          content=""
+        />
+        <meta
+          name="twitter:image"
+          content=""
+        />
+      </Head>
+      <main className="">
+        {photoId && (
+          <Modal
+            images={images}
+            onClose={() => {
+              setLastViewedPhoto(photoId)
+            }}
+          />
+        )}
+        <>
+          {/* Put your header here. */}
+          <div className="flex min-h-full flex-col font-sans text-zinc-900 bg-zinc-50 dark:text-zinc-100 dark:bg-black"><div className="text-center">
+            Header  </div>
+
+
+
+            <section>
+              <div className="max-w-screen-3xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                <section className="shadow-lg">
+
+                  Content
+
+                  {/* Put the rest of your page here. */}
+                </section>
+                <div className="text-center mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+                  Cta Banner
+
+                  <div>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+
+                      {images.map(({ id, public_id, format, blurDataUrl }) => (
+                        <Link
+                          key={id}
+                          href={`/?photoId=${id}`}
+                          as={`/p/${id}`}
+                          id={`photo-${id}`}
+                          shallow
+                          className="after:content group relative cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+                        >
+                          <Image
+                            alt="Next.js Conf photo"
+                            className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                            style={{ transform: 'translate3d(0, 0, 0)' }}
+                            placeholder="blur"
+                            blurDataURL={blurDataUrl}
+                            src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                            width={720}
+                            height={480}
+                            sizes="(max-width: 640px) 100vw,
+                 (max-width: 1280px) 50vw,
+                 (max-width: 1536px) 33vw,
+                 25vw"
+                          />
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+                  <div className="">
+
+
+                    <article className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                      <div className="text-center">
+                        content
+
+                      </div>
+                    </article>
+                  </div>
+                </main>
+                <div className="text-center mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+                  Header  </div>
+                <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+                  <div className="">
+
+
+                    <article className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                      <div className="text-center">
+                        content
+                      </div>
+                    </article>
+                  </div>
+                </main>
+
+              </div>
+            </section>
+
+
+
+          </div>
+        </>
+      </main>
+
+    </>
+  )
+}
+
+
+
+export default Home
+
+export async function getStaticProps() {
+  const results = await cloudinary.v2.search
+    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+    .sort_by('public_id', 'desc')
+    .max_results(400)
+    .execute()
+  let reducedResults: ImageProps[] = []
+
+  let i = 0
+  for (let result of results.resources) {
+    reducedResults.push({
+      id: i,
+      height: result.height,
+      width: result.width,
+      public_id: result.public_id,
+      format: result.format,
+    })
+    i++
+  }
+
+  const blurImagePromises = results.resources.map((image: ImageProps) => {
+    return getBase64ImageUrl(image)
+  })
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises)
+
+  for (let i = 0; i < reducedResults.length; i++) {
+    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i]
+  }
+
+  return {
+    props: {
+      images: reducedResults,
+    },
+  }
+}
