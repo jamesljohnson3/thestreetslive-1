9cf8e27063f5d5ba523c877cb6e8dc39dcb52652
Admin <support@unlimitpotential.com>
2023-12-23 15:11:08 -0500

    j

---
+++ src/components/SharedModal.tsx
@@ -0,0 +1,278 @@
+import {
+  ArrowDownTrayIcon,
+  ArrowTopRightOnSquareIcon,
+  ArrowUturnLeftIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { AnimatePresence, motion, MotionConfig } from 'framer-motion'
+import Image from 'next/image'
+import { useState } from 'react'
+import { useSwipeable } from 'react-swipeable'
+import { variants } from '../utils/animationVariants'
+import downloadPhoto from '../utils/downloadPhoto'
+import { range } from '../utils/range'
+import type { ImageProps, SharedModalProps } from '../utils/types'
+import Twitter from './Icons/Twitter'
+
+export default function SharedModal({
+  index,
+  images,
+  changePhotoId,
+  closeModal,
+  navigation,
+  currentPhoto,
+  direction,
+}: SharedModalProps) {
+  const [loaded, setLoaded] = useState(false)
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  // Function to show the snackbar
+  const showSnackbar = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarVisible(true);
+
+    // Hide the snackbar after a certain duration (e.g., 3 seconds)
+    setTimeout(() => {
+      setSnackbarVisible(false);
+      setSnackbarMessage('');
+    }, 1000);
+  };
+  let filteredImages = images?.filter((img: ImageProps) =>
+    range(index - 15, index + 15).includes(img.id)
+  )
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      if (index < images?.length - 1) {
+        changePhotoId(index + 1)
+      }
+    },
+    onSwipedRight: () => {
+      if (index > 0) {
+        changePhotoId(index - 1)
+      }
+    },
+    trackMouse: true,
+  })
+
+  let currentImage = images ? images[index] : currentPhoto
+
+  return (
+    <MotionConfig
+      transition={{
+        x: { type: 'spring', stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      }}
+    >
+      <div
+        className="relative z-50 flex aspect-[3/2] w-full max-w-7xl items-center wide:h-full xl:taller-than-854:h-auto"
+        {...handlers}
+      >
+        {/* Main image */}
+        <div className="w-full overflow-hidden">
+          <div className="relative flex aspect-[3/2] items-center justify-center">
+            <AnimatePresence initial={false} custom={direction}>
+              <motion.div
+                key={index}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                className="absolute"
+              >
+                <Image
+                  src={`https://res.cloudinary.com/${
+                    process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+                  }/image/upload/c_scale,${navigation ? 'w_1280' : 'w_1920'}/${
+                    currentImage.public_id
+                  }.${currentImage.format}`}
+                  width={navigation ? 1280 : 1920}
+                  height={navigation ? 853 : 1280}
+                  priority
+                  alt="Next.js Conf image"
+                  onLoadingComplete={() => setLoaded(true)}
+                />
+              </motion.div>
+            </AnimatePresence>
+          </div>
+        </div>
+          <div className="fixed inset-x-0 bottom-20 z-50 flex items-center justify-center">
+          <form
+      className="mt-20 rounded-2xl border bg-white border-zinc-100 p-6 dark:border-zinc-700/40"
+      action="/thank-you"
+    >
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          aria-hidden="true"
+          className="h-6 w-6 flex-none"
+        >
+          <path
+            d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+            className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+          ></path>
+          <path
+            d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
+            className="stroke-zinc-400 dark:stroke-zinc-500"
+          ></path>
+        </svg>
+        <span className="ml-3">Stay up to date</span>
+      </h2>
+      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        Get notified when I publish something new, and unsubscribe at any time.
+      </p>
+      <div className="mt-6 flex">
+        <input
+          type="email"
+          placeholder="Email address"
+          aria-label="Email address"
+          required
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+        />
+        <button
+          className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70 ml-4 flex-none"
+          type="submit"
+        >
+          Join
+        </button>
+      </div>
+    </form>
+          </div>
+        {/* Buttons + bottom nav bar */}
+        <div className="absolute inset-0 mx-auto flex max-w-7xl items-center justify-center">
+          {/* Buttons */}
+          {loaded && (
+            <div className="relative aspect-[3/2] max-h-full w-full">
+              {navigation && (
+                <>
+                  {index > 0 && (
+                    <button
+                      className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+                      style={{ transform: 'translate3d(0, 0, 0)' }}
+                      onClick={() => changePhotoId(index - 1)}
+                    >
+                      <ChevronLeftIcon className="h-6 w-6" />
+                    </button>
+                  )}
+                  {index + 1 < images.length && (
+                    <button
+                      className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+                      style={{ transform: 'translate3d(0, 0, 0)' }}
+                      onClick={() => changePhotoId(index + 1)}
+                    >
+                      <ChevronRightIcon className="h-6 w-6" />
+                    </button>
+                  )}
+                </>
+              )}
+              <div className="absolute top-0 right-0 flex items-center gap-2 p-3 text-white">
+                {navigation ? (
+                  <a
+                    href={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${currentImage.public_id}.${currentImage.format}`}
+                    className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                    target="_blank"
+                    title="Open fullsize version"
+                    rel="noreferrer"
+                  >
+                    <ArrowTopRightOnSquareIcon className="h-5 w-5" />
+                  </a>
+                ) : (
+                  <a
+                    href={`https://twitter.com/intent/tweet?text=Check%20out%20this%20pic%20from%20Next.js%20Conf!%0A%0Ahttps://nextjsconf-pics.vercel.app/p/${index}`}
+                    className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                    target="_blank"
+                    title="Open fullsize version"
+                    rel="noreferrer"
+                  >
+                    <Twitter className="h-5 w-5" />
+                  </a>
+                )}
+                <button
+                  onClick={() =>
+                    downloadPhoto(
+                      `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${currentImage.public_id}.${currentImage.format}`,
+                      `${index}.jpg`
+                    )
+                  }
+                  className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                  title="Download fullsize version"
+                >
+                  <ArrowDownTrayIcon className="h-5 w-5" />
+                </button>
+              </div>
+              <div className="absolute top-0 left-0 flex items-center gap-2 p-3 text-white">
+                <button
+                  onClick={() => closeModal()}
+                  className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                >
+                  {navigation ? (
+                    <XMarkIcon className="h-5 w-5" />
+                  ) : (
+                    <ArrowUturnLeftIcon className="h-5 w-5" />
+                  )}
+                </button>
+              </div>
+            </div>
+          )}
+          {/* Snackbar */}
+       
+          {/* Bottom Nav bar */}
+          {navigation && (
+            <div className="fixed inset-x-0 bottom-0 z-40 overflow-hidden bg-gradient-to-b from-black/0 to-black/60">
+              <motion.div
+                initial={false}
+                className="mx-auto mt-6 mb-6 flex aspect-[3/2] h-14"
+              >
+                <AnimatePresence initial={false}>
+                  {filteredImages.map(({ public_id, format, id }) => (
+                    <motion.button
+                      initial={{
+                        width: '0%',
+                        x: `${Math.max((index - 1) * -100, 15 * -100)}%`,
+                      }}
+                      animate={{
+                        scale: id === index ? 1.25 : 1,
+                        width: '100%',
+                        x: `${Math.max(index * -100, 15 * -100)}%`,
+                      }}
+                      exit={{ width: '0%' }}
+                      onClick={() => changePhotoId(id)}
+                      key={id}
+                      className={`${
+                        id === index
+                          ? 'z-20 rounded-md shadow shadow-black/50'
+                          : 'z-10'
+                      } ${id === 0 ? 'rounded-l-md' : ''} ${
+                        id === images.length - 1 ? 'rounded-r-md' : ''
+                      } relative inline-block w-full shrink-0 transform-gpu overflow-hidden focus:outline-none`}
+                    >
+                      <Image
+                        alt="small photos on the bottom"
+                        width={180}
+                        height={120}
+                        className={`${
+                          id === index
+                            ? 'brightness-110 hover:brightness-110'
+                            : 'brightness-50 contrast-125 hover:brightness-75'
+                        } h-full transform object-cover transition`}
+                        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_180/${public_id}.${format}`}
+                      />
+                    </motion.button>
+                  ))}
+                </AnimatePresence>
+              </motion.div>
+            </div>
+          )}
+        </div>
+      </div>
+    </MotionConfig>
+  )
+}
