f480f687fdda7c840eed9f9fb62b0d56fca9d46b
Admin <support@unlimitpotential.com>
2023-12-23 17:43:05 -0500

    j

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -28,17 +28,6 @@ const App = ({ Component, pageProps }) => {
 
 
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ReactGA.pageview(url);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <SessionProvider session={pageProps.session}>
