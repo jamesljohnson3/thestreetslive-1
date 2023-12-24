f6a434f975bb8f2d6b21fefbe773c1e4a93cf819
Admin <support@unlimitpotential.com>
2023-12-23 21:07:27 -0500

    h

--- src/pages/_sites/[site]/preview/index.tsx
+++ src/pages/_sites/[site]/preview/index.tsx
@@ -3,6 +3,7 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Carousel from '../../../../components/Carousel';
 
 const PhotoNotFound: React.FC = () => {
     return (
@@ -22,9 +23,14 @@ const PhotoPreviewPage: NextPage = () => {
         return <PhotoNotFound />;
     }
 
-    // Render the larger view of the photo
+    const currentPhoto = {
+        id: 1, // Replace with the actual photo ID
+        blurDataUrl: 'url-to-blurred-image', // Replace with the actual blurred image URL
+    };
+
     return (
         <div>
+            {/* Render the larger view of the photo */}
             <Image
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${id}`}
                 className="pointer-events-none object-contain h-full w-full"
@@ -33,6 +39,9 @@ const PhotoPreviewPage: NextPage = () => {
                     objectFit: 'cover', // cover, contain, none
                 }}
             />
+
+            {/* Render the Carousel component with the blurred background */}
+            <Carousel index={0} currentPhoto={currentPhoto} />
         </div>
     );
 };
