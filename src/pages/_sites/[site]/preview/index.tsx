dcfc36385e024a9e63e7abae1fa63131f0327d89
Admin <support@unlimitpotential.com>
2023-12-23 20:55:21 -0500

    h

--- src/pages/_sites/[site]/preview/index.tsx
+++ src/pages/_sites/[site]/preview/index.tsx
@@ -1,34 +1,39 @@
-// PhotoDetailPage.tsx
+// PhotoPreviewPage.tsx
 
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-const PhotoDetailPage: NextPage = () => {
-    const router = useRouter();
-    const { photoId } = router.query;
-
-    // Fetch the photo details using photoId and display them
-
+const PhotoNotFound: React.FC = () => {
     return (
         <div>
-            {/* Display the larger view of the clicked photo */}
-            {photoId && (
-                <div>
-                    {/* Fetch the photo details using photoId and display them */}
-                    <Image
-                        // Use the appropriate properties to display the larger view of the photo
-                        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${photoId}`}
-                        className="pointer-events-none h-full w-full"
-                        sizes="(min-width: 808px) 50vw, 100vw"
-                        style={{
-                            objectFit: 'cover', // cover, contain, none
-                        }}
-                    />
-                </div>
-            )}
+            <h1>Photo Not Found</h1>
+            <p>Sorry, the requested photo could not be found.</p>
         </div>
     );
 };
 
-export default PhotoDetailPage;
+const PhotoPreviewPage: NextPage = () => {
+    const router = useRouter();
+    const { photoId } = router.query;
+
+    // Check if photoId exists and render the larger view
+    if (photoId) {
+        return (
+            <div>
+                <Image
+                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${photoId}`}
+                    className="pointer-events-none h-full w-full"
+                    sizes="(min-width: 808px) 50vw, 100vw"
+                    style={{
+                        objectFit: 'cover', // cover, contain, none
+                    }}
+                />
+            </div>
+        );
+    }
+
+    return <PhotoNotFound />;
+};
+
+export default PhotoPreviewPage;
