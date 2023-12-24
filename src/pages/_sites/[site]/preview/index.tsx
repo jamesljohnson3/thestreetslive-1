31a0001d20d7134efeb1b307950db94d44b5e5bc
Admin <support@unlimitpotential.com>
2023-12-23 21:33:17 -0500

    rr

--- src/pages/_sites/[site]/preview/index.tsx
+++ src/pages/_sites/[site]/preview/index.tsx
@@ -24,16 +24,76 @@ const PhotoPreviewPage: NextPage = () => {
 
     // Render the larger view of the photo
     return (
-        <div>
-            <Image
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${id}`}
-                className="pointer-events-none h-full w-full"
-                sizes="(min-width: 808px) 50vw, 100vw"
-                style={{
-                    objectFit: 'contain', // cover, contain, none
-                }}
-            />
-        </div>
+
+        <>      {/* Put your header here. */}
+            <div className="flex min-h-full flex-col font-sans text-zinc-900 bg-zinc-50 dark:text-zinc-100 dark:bg-black"><div className="text-center">
+                Header  </div>
+
+
+
+                <section>
+                    <div className="max-w-screen-3xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                        <section className="shadow-lg">
+
+                            <div>
+                                <Image
+                                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${id}`}
+                                    className="pointer-events-none h-full w-full"
+                                    sizes="(min-width: 808px) 50vw, 100vw"
+                                    style={{
+                                        objectFit: 'contain', // cover, contain, none
+                                    }}
+                                />
+                            </div>
+
+                            {/* Put the rest of your page here. */}
+                        </section>
+                        <div className="text-center mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+                            Cta Banner
+
+                            <div>
+                                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+
+
+                                </div>
+                            </div>
+                        </div>
+                        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+                            <div className="">
+
+
+                                <article className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                                    <div className="text-center">
+                                        content
+
+                                    </div>
+                                </article>
+                            </div>
+                        </main>
+                        <div className="text-center mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+                            Header  </div>
+                        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+                            <div className="">
+
+
+                                <article className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                                    <div className="text-center">
+                                        content
+                                    </div>
+                                </article>
+                            </div>
+                        </main>
+
+                    </div>
+                </section>
+
+
+
+            </div>
+
+        </>
     );
 };
 
